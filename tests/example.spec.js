e6d5bbf248c2e505a0e8bcd2b414ab96689a2207
sruthi <sruthikg2010@gmail.com>
2026-07-05 01:25:14 +0100

    added a line in line12

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,9 +6,11 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  
 });
 
 test('get started link', async ({ page }) => {
+  //test statement
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
